42f5b301edf6e7a08270fc8f740fb4ffb98b82e6
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2015-06-02 22:28:38 +0900

    Fix test failure

--- test/search.tokenize.js
+++ test/search.tokenize.js
@@ -1,20 +1,20 @@
-var Search = require('../lib/search')
-  , search = new Search()
-  , assert = require('assert');
+var Search = require('../lib/search');
+var search = new Search()
+var assert = require('assert');
 
 describe('search.tokenize()', function() {
   it('should support uni-gram', function() {
-    var terms = search.tokenize('hello', 1)
-    terms.should.include('h').with.lengthOf(5);
+    var terms = search.tokenize('hello', 1);
+    assert.deepEqual(terms, [ 'h', 'e', 'l', 'l', 'o' ]);
   });
 
   it('should support bi-gram', function() {
-    var terms = search.tokenize('hello', 2)
-    terms.should.include('he').with.lengthOf(4);
+    var terms = search.tokenize('hello', 2);
+    assert.deepEqual(terms, [ 'he', 'el', 'll', 'lo' ]);
   });
 
-  it('should support text with space', function() {
-    var terms = search.tokenize('hello world', 2)
-    terms.should.include('he').with.lengthOf(8);
+  it('should ignore spaces', function() {
+    var terms = search.tokenize('hello world', 2);
+    assert.deepEqual(terms, [ 'he', 'el', 'll', 'lo', 'wo', 'or', 'rl', 'ld' ]);
   });
 });
